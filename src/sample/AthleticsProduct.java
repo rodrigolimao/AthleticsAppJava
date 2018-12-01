22a0c15ca2616b6948a38861cb1f075146d2b33c
Rodrigo <rodrigoclimao@gmail.com>
2018-11-30 23:38:47 -0500

    Initial commit

---
+++ src/sample/AthleticsProduct.java
@@ -0,0 +1,111 @@
+package sample;
+
+import javafx.scene.image.Image;
+
+public class AthleticsProduct {
+    private String productName, colour, sport;
+    private int units;
+    private double price;
+    private Image imageFile;
+
+    public AthleticsProduct(String productName, String colour, String sport, double price, int units, Image imageFile) {
+        setProductName(productName);
+        setColour(colour);
+        setColour(sport);
+        setPrice(price);
+        setUnits(units);
+        setImageFile(imageFile);
+    }
+
+    public AthleticsProduct(String productName, String colour, String sport, double price, int units) {
+        setProductName(productName);
+        setColour(colour);
+        setColour(sport);
+        setPrice(price);
+        setUnits(units);
+        setImageFile(imageFile);
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        if (!productName.isEmpty()) {
+            this.productName = productName;
+        } else {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        if (!colour.isEmpty()) {
+            this.colour = colour;
+        } else {
+            throw new IllegalArgumentException("Colour cannot be empty");
+        }
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        if (!sport.isEmpty()) {
+            this.sport = sport;
+        } else {
+            throw new IllegalArgumentException("Sport cannot be empty");
+        }
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price >= 0.1 && price <= 10000) {
+            this.price = price;
+        } else {
+            throw new IllegalArgumentException("Price shoud be between 0.1 and 10000");
+        }
+
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public void setUnits(int units) {
+        if (units >= 0.1 && units <= 10000) {
+            this.units = units;
+        } else {
+            throw new IllegalArgumentException("Units shoud be between 0.1 and 10000");
+        }
+    }
+
+    public void sellUnits(int units) {
+        if (units > 0) {
+            this.units = units - 1;
+        } else {
+            throw new IllegalArgumentException("No units in stock");
+        }
+    }
+
+    public Image getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(Image imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    public String toString() {
+        return String.format("Product Name= %s, Price= %s, Units=%s", productName, price, units);
+    }
+
+}
+
---
+++ src/sample/Inventory2.java
@@ -0,0 +1,82 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Inventory2 {
+
+    public static TreeMap<String, LinkedList<AthleticsProduct>> athleticsTreeMap = new TreeMap<>();
+
+    public static void main(String[] args) {
+        loadProducts();
+       System.out.println(getAllCategory());
+        System.out.println(allProductsArrayList());
+        System.out.println(productsByCategory("Soccer"));
+    }
+
+    /*
+    Method to addProduct in the TreeMap created
+     */
+    public static void addProduct(String productName, String colour, String sport, double price, int units) {
+        String key = sport;
+        AthleticsProduct newAthleticsProduct = new AthleticsProduct(productName, colour, sport, price, units);
+        if (athleticsTreeMap.containsKey(key)) {
+            athleticsTreeMap.get(key).add(newAthleticsProduct);
+        } else {
+            LinkedList<AthleticsProduct> linkedList = new LinkedList<>();
+            linkedList.add(newAthleticsProduct);
+            athleticsTreeMap.put(sport, linkedList);
+        }
+    }
+
+    /*
+    Method to load the Products created in the TreeMap
+     */
+    public static void loadProducts() {
+        addProduct("Short", "Blue", "Soccer", 15, 20);
+        addProduct("Shirt", "Blue", "Soccer", 15, 20);
+        addProduct("Socks", "Blue", "Soccer", 5, 20);
+        addProduct("cleats", "Black", "Soccer", 100, 20);
+        addProduct("Short", "Yellow", "Basketball", 15, 20);
+        addProduct("Shirt", "Blue", "Basketball", 15, 20);
+        addProduct("Socks", "Yellow", "Basketball", 10, 20);
+        addProduct("Shoes", "Black", "Basketball", 200, 20);
+        addProduct("Hat", "Yellow", "Cheerleader", 10, 20);
+        addProduct("Cheer Shirt", "Blue", "Cheerleader", 10, 20);
+        addProduct("Short Doll", "Yellow", "Cheerleader", 25, 20);
+        addProduct("Socks", "Blue", "Cheerleader", 10, 20);
+    }
+
+    /*
+    Return a List with all products for a specific category
+     */
+    public static LinkedList<AthleticsProduct> productsByCategory(String category) {
+        return athleticsTreeMap.get(category);
+    }
+
+    /*
+    Return a List with ALL products in an ArrayList
+     */
+    public static ArrayList<AthleticsProduct> allProductsArrayList() {
+        ArrayList<AthleticsProduct> arrayList = new ArrayList<>();
+        for (String chave : athleticsTreeMap.keySet()) {
+            arrayList.addAll(athleticsTreeMap.get(chave));
+        }
+        return arrayList;
+    }
+
+    /*
+    Return ALL category names
+     */
+    public static TreeMap<String, LinkedList<AthleticsProduct>> getAllCategory() {
+        for (Map.Entry<String, LinkedList<AthleticsProduct>> entry : athleticsTreeMap.entrySet()) {
+            String key = entry.getKey();
+            System.out.println(key);
+        }
+        return athleticsTreeMap;
+    }
+}
+
+
